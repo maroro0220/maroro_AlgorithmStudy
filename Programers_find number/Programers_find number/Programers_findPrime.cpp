415f0b6b5576fe1336aa7f058f375990ea817534
maroro0220 <30305125+maroro0220@users.noreply.github.com>
2019-09-03 16:36:12 +0900

    edit
    edit prime check
    but time over

--- Programers_find number/Programers_find number/Programers_findPrime.cpp
+++ Programers_find number/Programers_find number/Programers_findPrime.cpp
@@ -11,17 +11,8 @@ int visit[9999999];
 int prime[9999999];
 void dfs(int len, int select);
 int change(string chnum);
-/*
-bool chk_Prime(string chP) {
-	bool ret = true;
-	int ch_st=change(chP);
-	for (int a = 2; a < ch_st; a++) {
-		if (ch_st % a == 0) {
-			ret = false;
-		}//end if
-	}//end for chk Prime
-	return ret;
-}*/
+
+
 int solution(string numbers) {
 	int answer = 0;
 	cnt = 0;
@@ -43,7 +34,7 @@ void dfs(int len, int select) {
 	//int ch_stmp = change(str_in);
 	if (len == select) {//len is numbers length, select is selected number length
 		int ch_stmp = change(stmp);
-		if (ch_stmp == 1 ||ch_stmp==0) return;
+		if (ch_stmp == 1 ||ch_stmp==0||prime[ch_stmp]==1) return;
 		bool isPrime = true;
 		for (int a = 2; a < ch_stmp; a++) {
 			if (ch_stmp % a == 0) {
@@ -59,17 +50,11 @@ void dfs(int len, int select) {
 		}//end if
 	}//end if (len == select)
 	for (int i = 0; i < str_in.size(); i++) {
-		//if(!visit[str_in.at(i)])
 		if(!visit[i])
 		{
 			stmp.push_back(str_in.at(i));
-			//printf("stmp:%s\n", stmp.c_str());
-			//visit[str_in.at(i)] = 1;
-			//visit[atoi(stmp.c_str())] = 1;
 			visit[i] = 1;
 			dfs(len, i+1);
-			//visit[str_in.at(i)] = 0;
-			//visit[atoi(stmp.c_str())] = 0;
 			visit[i] = 0;
 			stmp.pop_back();
 		}
@@ -79,7 +64,6 @@ void dfs(int len, int select) {
 }
 int main() {
 	char tmp;
-//	string tmp_str;
 /*	
 while(1){
 		scanf("%c", &tmp);
@@ -90,9 +74,6 @@ while(1){
 		}
 	}*/
 	  cin >> str_in;
-	//	numbers.push_back(tmp_str);
-	//  printf("%d", str_in.size());
-	
 	printf("ans:%d", solution(str_in));
 
 	return 0;
