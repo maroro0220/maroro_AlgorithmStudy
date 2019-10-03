50557bd24969a6b053dcdae5d955a6356bfc000f
maroro0220 <30305125+maroro0220@users.noreply.github.com>
2019-10-03 17:10:07 +0900

    BOJ_9466_TermProj_DFS
    BOJ_9466_TermProj_DFS
    Now TIme over
    Test case working

---
+++ BOJ_9466_TeamProj_DFS/BOJ_9466_TeamProj_DFS/BOJ_9466_TeamProj_DFS.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> stud;
+vector<int> chk;
+int start;
+bool dfs(int n) {
+	if (n == start) {
+
+		return true;
+	}
+	if (!chk[n]) {
+		chk[n] = 1;
+		if (dfs(stud[n])) {
+			return true;
+		}
+		else
+			chk[n] = 0;
+	}
+	//chk[n] = 0;
+	return false;
+
+}
+int main() {
+	int T, n;
+	cin >> T;
+	for (int t = 0; t < T; t++) {//Test case Num
+		cin >> n;
+		chk = vector<int>(n + 1, 0);
+		stud = vector<int>(n + 1, 0);
+		for (int j = 1; j <=n;j++) {
+			cin >> stud[j];
+		}
+		for (int i = 1; i <= n; i++) {
+			
+			if (!chk[i]) {
+				start = i;
+				chk[i] = 1;
+				if (dfs(stud[i])) {
+					//continue;
+				}
+				else chk[i] = 0;
+			}
+		}
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (!chk[i]) {
+				cnt++;
+			}
+		}
+		cout << cnt << endl;
+	}
+	return 0;
+}
