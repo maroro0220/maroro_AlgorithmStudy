add471ea2e8c2d250bb79b2b94b2339812dd5b9d
maroro0220 <maro1339@gmail.com>
2019-10-15 15:19:00 +0900

    BOJ_9095_1,2,3Plus_DP
    BOJ_9095_1,2,3Plus_DP
    Easy

---
+++ BOJ_9095_1,2,3Plus_DP/BOJ_9095_1,2,3Plus_DP/BOJ_9095_1,2,3Plus_DP.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int n,T;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		vector<int> dp(n + 1, 0);
+		dp[1] = 1; dp[2] = 2; dp[3] = 4;
+		for (int i = 4; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		cout << dp[n]<<endl;
+	}
+	return 0;
+}
