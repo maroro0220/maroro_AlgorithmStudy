b0723a652ea7d192b8e34cc448c6dc2123b1a812
maroro0220 <maro1339@gmail.com>
2019-11-21 18:23:27 +0900

    Similar previous method
    Similar previous method

--- Programmers_CardGame_DP/Programmers_CardGame_DP/Programmers_CardGame_DP.cpp
+++ Programmers_CardGame_DP/Programmers_CardGame_DP/Programmers_CardGame_DP.cpp
@@ -2,13 +2,26 @@
 #include <vector>
 #include<algorithm>
 using namespace std;
-
+int Find(vector<int> card) {
+	int maxi=0,maxi_idx=0;
+	for (int i = 0; i < card.size(); i++) {
+		if (maxi < card[i]) { maxi = card[i]; maxi_idx = i; }
+	}
+	return maxi_idx;
+}
 int solution(vector<int> left, vector<int> right) {
 	int answer = 0;
-	sort(left.begin(), left.end(), greater<int>());//Descending order
-	int left_max = left[0];
+	//sort(left.begin(), left.end(), greater<int>());//Descending order
+	//int left_max = left[0];
+	int left_max_idx = Find(left);
 	for (int i = 0; i < right.size(); i++) {
-		if (left_max > right[i]) answer += right[i];
+		if (left[left_max_idx] > right[i]) { 
+			answer += right[i]; 
+		}
+		else {
+			left[left_max_idx] = 0; right[i] = 0;
+			left_max_idx = Find(left);
+		}
 	}
 	/*
 	int l_idx =0, r_idx = 0;
