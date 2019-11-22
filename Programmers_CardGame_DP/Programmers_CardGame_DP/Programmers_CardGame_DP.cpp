f2f612935d94d8c2d527b0fef3f13db8ccf23032
maroro0220 <maro1339@gmail.com>
2019-11-22 15:47:06 +0900

    Using DFS DP
    Using 'DFS - DP'
    It's slower than 'for for - DP'

--- Programmers_CardGame_DP/Programmers_CardGame_DP/Programmers_CardGame_DP.cpp
+++ Programmers_CardGame_DP/Programmers_CardGame_DP/Programmers_CardGame_DP.cpp
@@ -4,21 +4,32 @@
 #include<iostream>
 using namespace std;
 vector<vector<int>>dp;
+vector<int> l, r;
+int dfs(int lef, int righ) {
+	if (lef == l.size() || righ == r.size()) {
+		return 0;
+	}
+	if (dp[lef][righ] != -1) {
+		return dp[lef][righ];
+	}
+	dp[lef][righ] = max(dfs(lef + 1, righ), dfs(lef + 1, righ + 1));
+	if (l[lef] > r[righ]) {
+		dp[lef][righ] = dfs(lef , righ + 1)+r[righ];
+	}
+	return dp[lef][righ];
+}
 int solution(vector<int> left, vector<int> right) {
 	int answer = 0;
-	dp = vector<vector<int>>(left.size() + 1, vector<int>(right.size() + 1, 0));
-
-	//for (int i = 1; i <= left.size() + 1; i++) {	dp[0][i] = -1;}
+	dp = vector<vector<int>>(left.size() + 1, vector<int>(right.size() + 1, -1));
+	l = left;
+	r = right;
+	/*
 	for (int i = 0; i < left.size(); i++) {
 		for (int j = 0; j < right.size(); j++) {
-			//cout << "i: " << i << "j: " <<j<< endl;
-			//cout << "before dp i+1 j+1 => dp[i][j]:" << dp[i][j] << ' ' << endl;
 			dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i][j]);
-			//cout << "1after dp i+1 j+1: " << dp[i + 1][j + 1] << endl;
 			if (left[i] > right[j]) {
 				dp[i + 1][j + 1] = dp[i + 1][j] + right[j];
 			}
-			//cout << "2after dp i+1 j+1: " << dp[i + 1][j + 1] << endl;
 
 		}
 		cout << endl;
@@ -26,6 +37,8 @@ int solution(vector<int> left, vector<int> right) {
 	for (int i = 0; i < right.size() + 1; i++) {
 		if (answer < dp[left.size()][i]) answer = dp[left.size()][i];
 	}
+	*/
+	answer = dfs(0, 0);
 	return answer;
 }
 
