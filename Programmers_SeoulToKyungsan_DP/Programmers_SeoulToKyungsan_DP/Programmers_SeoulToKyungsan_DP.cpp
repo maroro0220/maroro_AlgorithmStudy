11056483f1282bfa81aa2586763ee9a897712b5a
maroro0220 <maro1339@gmail.com>
2019-11-27 00:38:11 +0900

    Programmers_SeoulToKyungsan_DP,DFS(On going)
    Programmers_SeoulToKyungsan_DP,DFS(On going)

--- Programmers_SeoulToKyungsan_DP/Programmers_SeoulToKyungsan_DP/Programmers_SeoulToKyungsan_DP.cpp
+++ Programmers_SeoulToKyungsan_DP/Programmers_SeoulToKyungsan_DP/Programmers_SeoulToKyungsan_DP.cpp
@@ -2,21 +2,45 @@
 #include <vector>
 #include<algorithm>
 using namespace std;
-/*
-int dfs(vector<vector<int>> trav,vector<int> dp, int cnt, int time_sum, int kk) {
-	if (cnt == trav.size()) {
-		return 0;
+vector<vector<int>> dp;
+vector<vector<int>> trav;
+int kk;
+int dfs(int cnt, int time_sum,int money_sum) {
+	if (time_sum > kk) return -1;
+	if (cnt == trav.size()&&time_sum<=kk) {
+		return money_sum;
 	}
-	for (int i = 0; i < 4; i += 2) {
-		if (time_sum + trav[cnt][i] > kk) {
-			continue;
-		}
-		dp[cnt] = max(dp[cnt], dp[cnt - 1] + trav[cnt][i + 1]);
-		dp[cnt] = dfs(trav, dp , cnt + 1, time_sum + trav[cnt][i],kk);
+	if (dp[cnt][time_sum]) {
+		return dp[cnt][time_sum];
 	}
-	return dp[cnt-1];
+	if (dp[cnt][time_sum]==-1) {
+		//return -1;
+	}
+	int res = -1;
+	res = max(res, dfs(cnt + 1, time_sum + trav[cnt][0], money_sum + trav[cnt][1]));
+	res = max(res, dfs(cnt + 1, time_sum + trav[cnt][2], money_sum + trav[cnt][3]));
+	if (res) {
+		dp[cnt][time_sum] = res;
+	}
+	else if (res==-1) {
+		dp[cnt][time_sum] = 0;
+	}
+	return res;
 }
-*/
+int solution(int K, vector<vector<int>> travel) {
+	int answer = 0;
+	dp = vector<vector<int>>(travel.size(), vector<int>(K+1, 0));
+	kk = K;
+	trav = travel;
+	dp[0][travel[0][0]] = travel[0][1];
+	dp[0][travel[0][2]] = travel[0][3];
+	answer = dfs(1, travel[0][0], travel[0][1]);
+	answer = max(answer, dfs(1, travel[0][2], travel[0][3]));
+
+	return answer;
+}
+
+/*
 int solution(int K, vector<vector<int>> travel) {
 	int answer = 0;
 	vector<vector<int>> dp(travel.size(),vector<int>(K+1,0));
@@ -36,10 +60,11 @@ int solution(int K, vector<vector<int>> travel) {
 		}
 	}
 	return answer;
-}
+}*/
 int main() {
-
+	//660
 	printf("%d\n", solution(1650, { {500,200,200,100},{800, 370, 300, 120},{700, 250, 300, 90 } }));
+	//5900
 	printf("%d\n", solution(3000, { {1000, 2000, 300, 700},{1100, 1900, 400, 900},{900, 1800, 400, 700 }, { 1200, 2300, 500, 1200 }}));
 	return 0;
 }
