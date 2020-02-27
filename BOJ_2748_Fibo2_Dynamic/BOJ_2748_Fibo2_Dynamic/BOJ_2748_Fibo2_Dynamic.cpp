031027d4bca2030c2a1bbb205d39851df17a122f
maroro0220 <maro1339@gmail.com>
2020-02-27 10:47:22 +0900

    BaekJoon 2748 Fibonacci. Using recursive function and memoization

--- BOJ_2748_Fibo2_Dynamic/BOJ_2748_Fibo2_Dynamic/BOJ_2748_Fibo2_Dynamic.cpp
+++ BOJ_2748_Fibo2_Dynamic/BOJ_2748_Fibo2_Dynamic/BOJ_2748_Fibo2_Dynamic.cpp
@@ -1,16 +1,49 @@
-#include<iostream>
+
+/*
+피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. 그 다음 2번째 부터는 바로 앞 두 피보나치 수의 합이 된다.
+
+이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n>=2)가 된다.
+
+n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+
+n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 n이 주어진다. n은 90보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 n번째 피보나치 수를 출력한다.
+*/#include<iostream>
 #include<vector>
 using namespace std;
+int n;
 vector<long long>dp;
+long long dfs(int N) {
+	if (N == 1) {
+		return 1;
+	}
+	if (N == 0) {
+		return 0;
+	}
+	if (dp[N]) {
+		return dp[N];
+	}
+	else {
+		dp[N]= dfs(N - 1) + dfs(N - 2);
+	}
+	return dp[N];
+}
 int main() {
-	int n;
 	cin >> n;
-	dp = vector<long long>(n+1);
+	dp = vector<long long>(n+1,0);
 	dp[0] = 0; dp[1] = 1;
-
+	/*
 	for (int i = 2; i <dp.size(); i++) {
 		dp[i] = dp[i - 1] + dp[i - 2];
 	}
-	printf("%lld", dp[n]);
+	*/
+	printf("%lld", dfs(n));
 	return 0;
 }
