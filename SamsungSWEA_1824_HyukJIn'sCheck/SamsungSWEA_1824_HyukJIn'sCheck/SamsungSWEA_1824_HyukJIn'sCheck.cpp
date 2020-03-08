9b655e2c9ea5acff8a44e2330cbbc2581efff0bc
maroro0220 <maro1339@gmail.com>
2020-03-08 23:56:38 +0900

    SamsungSWEA 1824 HyukJin'Check _Simulation_fail Because of '?'. it's random. So I should check four direction. So I need to use recursive

--- SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck.cpp
+++ SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck.cpp
@@ -1,20 +1,22 @@
 
 #include<iostream>
 #include<vector>
-#include<cstdlib>
-#include<ctime>
 using namespace std;
+int r, c;
+vector<vector<char>>arr; 
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	int r, c;
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		cin >> r >> c;
-		vector<vector<char>>arr(r, vector<char>(c));
+		arr= vector<vector<char>>(r, vector<char>(c));
+		int mem[20][20][16][4] = { 0 };
+
+
 		for (int i = 0; i < r; i++) {
 			for (int j = 0; j < c; j++) {
 				cin >> arr[i][j];
@@ -35,14 +37,20 @@ int main(int argc, char** argv)
 		-	메모리에 저장된 값에 1을 뺀다. 만약 빼기 전 값이 0이라면 15로 바꾼다.
 		*/
 		int a=0, b=0;
-		int mem;
+		int data=0;
 		int dr[4] = { 0,0,-1,1 };
 		int dc[4] = { -1,1,0,0 };
 		int dir=1;
-		while (arr[a][b] != '@') {
-			
+		int qd = 0;
+		string res="NO";
+		
+		while (1) {
+			if (arr[a][b] == '@') {
+				res = "YES";
+				break;
+			}
 			if (arr[a][b] >= '0' && arr[a][b] <= '9') {
-				mem = arr[a][b] - '0';
+				data = arr[a][b] - '0';
 			}
 			else if (arr[a][b] == '<') {
 				dir = 0;
@@ -51,48 +59,92 @@ int main(int argc, char** argv)
 				dir = 1;
 			}
 			else if (arr[a][b] == '^') {
-				dir =2;
+				dir = 2;
 			}
 			else if (arr[a][b] == 'v') {
 				dir = 3;
 			}
-			else if (arr[a][b]=='_') {
-				if (mem == '0') {
+			else if (arr[a][b] == '_') {
+				if (!data) {
 					dir = 1;
 				}
-				else if (mem != '0') {
+				else {
 					dir = 0;
 				}
 			}
 			else if (arr[a][b] == '|') {
-				if (mem == '0') {
+				if (!data ) {
 					dir = 3;
 				}
-				else if (mem != '0') {
+				else if (data) {
 					dir = 2;
 				}
 			}
 			else if (arr[a][b] == '?') {
-				srand((unsigned int)time(NULL));
-				dir=int(rand()%4);
+				switch (qd)
+				{
+				case (0):
+					dir = 0;
+					break;
+				case (1):
+					dir = 1;
+					break;
+				case (2):
+					dir = 2;
+					break;
+				case (3):
+					dir = 3;
+					break;
+				default:
+					break;
+				}
+				if (qd == 3) qd = 0;
+				else qd ++ ;
 			}
 			else if (arr[a][b] == '.') {
-			//nothing
+				//nothing
 			}
-			else if (arr[a][b] == '+') {
-				if (mem == 15) {
-					mem = 0;
+			if (arr[a][b] == '+') {
+				if (data == 15) {
+					data = 0;
 				}
-				else mem++;
+				else data++;
 			}
 			else if (arr[a][b] == '-') {
-				if (mem == 0) {
-					mem = 15;
+				if (data == 0) {
+					data = 15;
 				}
-				else mem--;
+				else data--;
+			}
+			if (mem[a][b][data][dir]==5) {
+				cout << a << ' ' << b << ' ' << data << ' ' << dir << '=' << mem[a][b][data][dir] << endl;
+				
+				break;
+			}
+			else {
+				cout << a << ' ' << b << ' ' << data << ' ' << dir <<'='<< mem[a][b][data][dir] << endl;
+
+				mem[a][b][data][dir] ++;
+			}if (a + dr[dir] >= r) {
+				a = 0;
+			}
+			else if (a + dr[dir] < 0) {
+				a = r - 1;
+			}
+			else {
+				a += dr[dir];
+			}
+			if (b + dc[dir] >= c) {
+				b = 0;
+			}
+			else if (b + dc[dir] < 0) {
+				b = c - 1;
+			}
+			else {
+				b += dc[dir];
 			}
 		}
-
+		cout << '#' << test_case << ' ' << res << endl;
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
