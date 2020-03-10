17b71ad87fc7bc7e5dad6d3f70bfd68d5ecc7b1a
maroro0220 <maro1339@gmail.com>
2020-03-10 10:34:37 +0900

    SamsungSWEA

--- SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck.cpp
+++ SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck/SamsungSWEA_1824_HyukJIn'sCheck.cpp
@@ -165,3 +165,144 @@ int main(int argc, char** argv)
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
+
+/*
+//other people's
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int N, M;
+int chk[20][20][4][16];
+char map[20][20];
+int dx[4] = { 0, -1, 0, 1 };
+int dy[4] = { 1, 0, -1, 0 };
+int finished = 0;
+int qx[26000];
+int qy[26000];
+int qd[26000];
+int qn[26000];
+int qf[26000];
+int st, ed;
+int main() {
+
+	int test_case, t;
+	int i, j, k, l;
+
+	scanf("%d", &test_case);
+
+	for (t = 1; t <= test_case; ++t) {
+
+		finished = 0;
+		scanf("%d %d", &N, &M);
+		for (i = 0; i < N; ++i) {
+			for (j = 0; j < M; ++j) {
+				scanf(" %c", &map[i][j]);
+				for (k = 0; k < 4; ++k) {
+					for (l = 0; l <= 15; ++l) {
+						chk[i][j][k][l] = 0;
+					}
+				}
+			}
+		}
+		st = ed = 0;
+		qx[++st] = 0;
+		qy[st] = 0;
+		qd[st] = 0;
+		qn[st] = 0;
+		qf[st] = -1;
+		int x, y, d, n;
+		int now = 0;
+		char c;
+		while (1) {
+			ed++;
+			if (qx[ed] == 2 && qy[ed] == 4) {
+				now++;
+			}
+			if (map[qx[ed]][qy[ed]] == '@') {
+				printf("#%d YES\n", t);
+				break;
+			}
+			if (st < ed) {
+				printf("#%d NO\n", t);
+				break;
+			}
+			x = qx[ed];
+			y = qy[ed];
+			d = qd[ed];
+			n = qn[ed];
+			c = map[qx[ed]][qy[ed]];
+
+			if ('0' <= c && c <= '9') {
+				n = c - '0';
+			}
+			else if (c == '<') {
+				d = 2;
+			}
+			else if (c == '>') {
+				d = 0;
+			}
+			else if (c == 'v') {
+				d = 3;
+			}
+			else if (c == '^') {
+				d = 1;
+			}
+			else if (c == '_') {
+				if (n == 0) {
+					d = 0;
+				}
+				else {
+					d = 2;
+				}
+			}
+			else if (c == '|') {
+				if (n == 0) {
+					d = 3;
+				}
+				else {
+					d = 1;
+				}
+			}
+			else if (c == '+') {
+				n++;
+				if (n == 16) {
+					n = 0;
+				}
+			}
+			else if (c == '-') {
+				n--;
+				if (n == -1) {
+					n = 15;
+				}
+			}
+			int nx, ny;
+			if (c == '?') {
+				for (i = 0; i < 4; ++i) {
+					nx = (x + dx[i] + N) % N;
+					ny = (y + dy[i] + M) % M;
+					if (chk[nx][ny][i][n] == 0) {
+						chk[nx][ny][i][n] = 1;
+						qx[++st] = nx;
+						qy[st] = ny;
+						qd[st] = i;
+						qn[st] = n;
+						qf[st] = ed;
+					}
+				}
+			}
+			else {
+				nx = (x + dx[d] + N) % N;
+				ny = (y + dy[d] + M) % M;
+				if (chk[nx][ny][d][n] == 0) {
+					chk[nx][ny][d][n] = 1;
+					qx[++st] = nx;
+					qy[st] = ny;
+					qd[st] = d;
+					qn[st] = n;
+					qf[st] = ed;
+				}
+			}
+		}
+	}
+}
+
+*/
---
+++ SamsungSWEA_9280_Jinyong'sParkingTower/SamsungSWEA_9280_Jinyong'sParkingTower/SamsungSWEA_9280_Jinyong'sParkingTower.cpp
@@ -0,0 +1,71 @@
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n, m;
+		int cnt;
+		cin >> n >> m;
+		vector<int> space(n+1);
+		vector<int> car(m);
+		vector<int>chk(n+1, 0); //chk space
+		queue<int>q;
+		for (int i = 0; i < n; i++) {
+			cin >> space[i];
+		}
+		for (int i = 0; i < m; i++) {
+			cin >> car[i];
+		}
+		int tmp;
+		int res = 0;
+		cnt = 0;
+		while (cnt < m) {
+			
+			cin >> tmp;
+			if (tmp > 0) {
+				for (int i = 0; i <= n; i++) {
+					if (i == n) {
+						q.push(tmp);
+					}
+					else if (!chk[i]) {
+						res += car[tmp - 1] * space[i];
+						chk[i] = tmp;
+						break;
+					}
+					
+				}
+			}
+			else {
+				tmp *= -1;
+				for (int i = 0; i < n; i++) {
+					if (chk[i] == tmp) {
+						chk[i] = 0;
+						cnt++;
+					//	cout << "cnt "<<cnt<<endl;
+						if (!q.empty()) {
+							chk[i] = q.front();
+							res += car[q.front() - 1] * space[i];
+							q.pop();
+						}	
+						break;
+					}
+				}
+
+			}
+		}
+		cout <<'#'<<test_case<<' '<< res << endl;
+
+
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
