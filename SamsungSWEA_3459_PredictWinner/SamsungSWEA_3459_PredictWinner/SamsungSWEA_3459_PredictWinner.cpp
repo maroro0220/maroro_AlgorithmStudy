c09f3ee1d90d49ed069c556be07b431c8c25e60c
maroro0220 <maro1339@gmail.com>
2020-03-11 15:07:12 +0900

    SamsungSWEA 3459 Predict WInner little bit difficult. I should find rule.

--- SamsungSWEA_3459_PredictWinner/SamsungSWEA_3459_PredictWinner/SamsungSWEA_3459_PredictWinner.cpp
+++ SamsungSWEA_3459_PredictWinner/SamsungSWEA_3459_PredictWinner/SamsungSWEA_3459_PredictWinner.cpp
@@ -15,72 +15,47 @@ int main(int argc, char** argv)
 	{
 		long long N;
 		scanf("%lld", &N);
-		string res = "Bob";
-		
-		/*
-		long long x = 1;
-		long long cnt = 0;
-		queue<long long> q;
-		q.push(x);
-		while (!q.empty()) {
-			int s = q.size();
-			cout <<"qsize : "<< s << endl;
-			for (int i = 0; i < s; i++) {
-				long long tmp = q.front();
-				cout << "tmp: " << tmp << endl;
-				q.pop();
-				if ((tmp * 2 + 1 > N || tmp * 2 > N)) continue;
-				else {
-					q.push(tmp * 2 + 1);
-					q.push(tmp * 2);
-				}
+		string res;
+		if (N < 4) {
+			if (N == 1) {
+				printf("#%d Bob\n", test_case);//res = "Bob"; 
 			}
-			cnt++;
-			cout << "cnt: " << cnt << endl;
+			else printf("#%d Alice\n", test_case); //res = "Alice";
 		}
-		*/
-		/*
-		while (1) {
-			if ( (x * 2 + 1) > N||x * 2 > N ) {
-				break;
-			}
-			else if (x * 2 + 1 <= N) {
-				cnt++;
-				x = x * 2 + 1;
-			}
-			else if (x * 2 <= N) {
-				cnt++;
-				x *= 2;
-			}
-		}
-		if (cnt % 2 == 0) {
-			res = "Alice";
-		}
-		*/
-		/*
-		//Time over
-		long long depth = 0ll;
-		if (N == 1) { res = "Bob"; }
 		else {
-			depth = 1;
+			int depth = 2;
 			while (1) {
-				long long start, end;
-				start = (1ll << depth);
-				end = (start << 1);
-				//cout <<N<<' '<< start << ' ' << end<<endl;
-				if (N >= start && N < end) {
-					//depth++;
-					if (N < (start + end) / 2)res = "Alice";
-					else res = "Bob";
-					break;
+				if (N >= (1ll << depth) && N < (1ll << (depth + 1))) break;
+				depth++;
+			}
+			long long x = 1ll;
+			if (depth % 2) {
+				for (int i = 1; i <= depth; i++) {
+					if (i % 2) {
+						x =x<< 1;
+					}
+					else {
+						x = (x << 1) + 1;
+					}
+				}					
+				if (x <= N) printf("#%d Alice\n", test_case);//res = "Alice";
+				else printf("#%d Bob\n", test_case); //res = "Bob";
+			}
+			else {
+				for (int i = 1; i <= depth; i++) {
+					if (i % 2) {
+						x = (x << 1) + 1;
+					}
+					else {
+						x =x<< 1;
+					}
 				}
-				else {
-					depth++;
-				}
+				if (x > N) printf("#%d Alice\n", test_case);//res = "Alice";
+				else printf("#%d Bob\n", test_case);//res = "Bob";
 			}
 		}
-		*/
-		cout << '#' << test_case << ' ' << res<<endl;
+		
+		
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
