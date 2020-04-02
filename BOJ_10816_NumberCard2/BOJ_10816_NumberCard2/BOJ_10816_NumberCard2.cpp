b3e08e58e8d0a0a375e15cb2e9f81cadd7d2d059
maroro0220 <maro1339@gmail.com>
2020-04-02 17:12:10 +0900

    BOJ_10816_NumberCard2 BInary Search Using map also possible. But slow and get a lot of memories. And using vector is slow than int new[]

--- BOJ_10816_NumberCard2/BOJ_10816_NumberCard2/BOJ_10816_NumberCard2.cpp
+++ BOJ_10816_NumberCard2/BOJ_10816_NumberCard2/BOJ_10816_NumberCard2.cpp
@@ -1,30 +1,72 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<algorithm>
 using namespace std;
-
+int Up(int *arr,int t,int s){
+	int left = 0, right = s - 1;
+	int mid;
+	while (left < right) {
+		mid = (left + right) / 2;
+		if (arr[mid] > t) {
+			right = mid;
+		}
+		else left=mid+1;
+	}
+	return right;
+}
+int Low(int *arr, int t, int s) {
+	int left = 0, right = s - 1;
+	int mid;
+	while (left < right) {
+		mid = (left + right) / 2;
+		if (arr[mid] >= t) {
+			right = mid;
+		}
+		else left = mid + 1;
+	}
+	return right;
+}
 int main() {
 	int M, N;
-	cin >> N;
-	/*
-	//time over
-	map<int, int>num;
-	int tmp;
+	scanf("%d", &N);
+	//vector<int>sang(N);
+	int *sang = new int[N];
 	for (int i = 0; i < N; i++) {
-		cin >> tmp;
-		if (num.count(tmp)) {
-			num[tmp]++;
-			//num.insert(pair<int,int>(tmp, 1));
-		}
-		else {
-			num.insert(make_pair(tmp,1));
-			//num[tmp]=num[tmp]+1;
-		}
+		scanf("%d", &sang[i]);
 	}
-	cin >> M;
+	sort(sang, sang + N);
+	//sort(sang.begin(), sang.end());
+	scanf("%d",&M);
+	int tmp;
+	
+	int up, low;
 	for (int i = 0; i < M; i++) {
-		cin >> tmp;
-		if (num[tmp])printf("%d ", num[tmp]);
+		//cin >> tmp;
+		scanf("%d", &tmp);
+		up = Up(sang, tmp, N);
+		low = Low(sang, tmp, N);
+		//if (up - low) up++;
+		if (up == N - 1 && sang[N - 1] == tmp) { 
+			up++; 
+		}
+		printf("%d ", up-low );
+	}
+	
+	/*
+	//slow
+	map<int, int>num;
+	//int tmp;
+	for (int i = 0; i < N; i++) {
+		num.insert({ sang[i],0 });
+	}
+	for (int i = 0; i < N; i++) {
+		num.find(sang[i])->second++;
+	}
+	for (int i = 0; i < M; i++) {
+		
+		scanf("%d", &tmp);
+		if (num.count(tmp))printf("%d ", num.find(tmp)->second);
 		else printf("0 ");
 	}
 	*/
