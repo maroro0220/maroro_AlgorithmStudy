d4406ccea9699980297e0b15ec9669aea332610b
maroro0220 <maro1339@gmail.com>
2020-04-02 15:26:09 +0900

    SamsungSWEA_1868_Minesweeper I use DFS? Bruteforce?. little bit difficult

--- SamsungSWEA_1868_Minesweeper/SamsungSWEA_1868_Minesweeper/SamsungSWEA_1868_Minesweeper.cpp
+++ SamsungSWEA_1868_Minesweeper/SamsungSWEA_1868_Minesweeper/SamsungSWEA_1868_Minesweeper.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 using namespace std;
 vector<vector<char>> map;
+vector<vector<int>> chk_arr;
 int N;
 int dr[8] = {-1,-1,-1,0,1,1,1,0};
 int dc[8] = { -1,0,1,1,1,0,-1,-1 };
@@ -18,11 +19,9 @@ int Chk(int r, int c) {
 	return cnt;
 }
 int dfs(int r,int c,int cnt) {
-	//if (r == N - 1 && c == N - 1) {
-	//	return cnt;
-	//}
 		int chk_ret = Chk(r, c);
 		map[r][c] = chk_ret+'0';
+		chk_arr[r][c] = 1;
 		if (chk_ret == 0) {
 			for (int i = 0; i < 8; i++) {
 				int next_r = r + dr[i];
@@ -32,29 +31,16 @@ int dfs(int r,int c,int cnt) {
 				if (map[next_r][next_c] == '.') {
 					int next_ret = Chk(next_r, next_c);
 					map[next_r][next_c] = next_ret + '0';
+				
 					if (next_ret == 0) {
-						//if (map[next_r][next_c] == '.')
 							dfs(next_r, next_c, cnt);
 					}
-					//else if (next_ret) {
-					//	map[next_r][next_c] = next_ret + '0';
-					//}
 				}
-				else if (map[next_r][next_c] >= '1' && map[next_r][next_c] < '9') { map[next_r][next_c] = '9'; ans--; }
+				if (chk_arr[next_r][next_c]&&map[next_r][next_c]!='0') {
+						ans--;
+						chk_arr[next_r][next_c] = 0;
+				}
 			}
-		
-		//else{
-		/*
-			for (int i = 0; i < 8; i++) {
-				int next_r = r + dr[i];
-				int next_c = c + dc[i];
-				if (next_r < 0 || next_r >= N || next_c < 0 || next_c >= N) continue;
-				if (map[next_r][next_c] == '*') continue;
-				if (map[next_r][next_c] == '.') dfs(next_r, next_c, cnt + 1);
-				//else if (map[next_r][next_c] >= '0'&& map[next_r][next_c] <= '8') dfs(next_r, next_c, cnt);
-			}
-		*/
-		//}
 	}
 	return cnt;
 }
@@ -67,6 +53,7 @@ int main(int argc, char** argv)
 	{
 		cin >> N;
 		map = vector<vector<char>>(N, vector<char>(N));
+		chk_arr = vector<vector<int>>(N, vector<int>(N,0));
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				cin >> map[i][j];
@@ -78,20 +65,11 @@ int main(int argc, char** argv)
 				if (map[i][j] == '.') {
 					ans++;
 					dfs(i, j,0);
-					//printf("%d %d\n", i, j);
 				}
 			}
 		}
 		printf("#%d %d\n",test_case, ans);
-		//printf("%d\n",dfs(0, 0,1));
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				printf("%c ", map[i][j]);
-			}
-			printf("\n");
-		}
-		
+
 		
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
