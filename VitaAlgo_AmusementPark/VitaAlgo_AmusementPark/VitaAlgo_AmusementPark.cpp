647433877efe0adc6d96423ab851bab672e0fffd
maroro0220 <maro1339@gmail.com>
2020-04-10 19:24:08 +0900

    Using DP. Not mine

--- VitaAlgo_AmusementPark/VitaAlgo_AmusementPark/VitaAlgo_AmusementPark.cpp
+++ VitaAlgo_AmusementPark/VitaAlgo_AmusementPark/VitaAlgo_AmusementPark.cpp
@@ -1,11 +1,16 @@
+
 #include <iostream>
+#include<algorithm>
+#include<cstring>
 using namespace std;
-int arr[17][17];
+int arr[16][16];
+int d[1 << 16][16];
 int chk[17];
 int n;
 int sum;
 int start;
 int mini = 100000000;
+/*
 void dfs(int idx,int cnt) {
 	if (cnt >= n && idx==start) {
 		if (mini > sum) mini = sum;
@@ -24,19 +29,36 @@ void dfs(int idx,int cnt) {
 		chk[idx] = 0;
 	}
 }
+*/
+int dfs(int cur,int st) {
+	if (st == (1 << n) - 1) {
+		return arr[cur][0] ? arr[cur][0] : 1000000000;
+	}
+	int& res = d[st][cur];
+	if (res != -1)return res;
+	res = 1000000000;
+	for (int i = 0; i < n; i++) {
+		if ((st & (1 << i)) == 0 && arr[cur][i]) {
+			res = min(res, dfs(i, st | (1 << i))+arr[cur][i]);
+		}
+	}
+	return res;
+}
 int main() {
-	cin >> n;
+	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
 		}
 	}
+	memset(d, -1, sizeof(d));
 	sum = 0;
 	//for (int i = 0; i < n; i++) {
-		
-		start =0;
-		dfs(0,0);
+	mini = dfs(0, 1);
+		//start =0;
+		//dfs(0,0);
 	//}
-	cout << mini;
+	//cout << mini;
+	printf("%d", mini);
 	return 0;
 }
