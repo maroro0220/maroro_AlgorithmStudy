6a8cdcc0f9a6d6fe3d82b0551d6c27b3f52306fd
maroro0220 <maro1339@gmail.com>
2020-04-21 13:01:35 +0900

    This code use lower bound and compressing array index.

--- GroomVitaAlgo_PrizeDraw/GroomVitaAlgo_PrizeDraw/GroomVitaAlgo_PrizeDraw.cpp
+++ GroomVitaAlgo_PrizeDraw/GroomVitaAlgo_PrizeDraw/GroomVitaAlgo_PrizeDraw.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	int n, m;
+	int* cnt;
+	scanf("%d %d",&n,&m);
+	int* arr = new int[n+1]();
+	cnt= new int[n]();
+	vector<int> numb;
+	for (int i = 0; i < n; i++) {
+		scanf("%d",&arr[i]);
+		numb.push_back(arr[i]);
+	}
+	sort(numb.begin(),numb.end());
+	numb.erase(unique(numb.begin(), numb.end()), numb.end());
+	for (int i = 0; i < n; i++) {
+		int idx = lower_bound(numb.begin(), numb.end(), arr[i]) - numb.begin();
+		cnt[idx]++;
+	}
+	for (int i = 0; i < m; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		if (binary_search(numb.begin(), numb.end(), tmp)) {
+			int idx = lower_bound(numb.begin(), numb.end(), tmp) - numb.begin();
+			printf("%d ", cnt[idx]);
+		}
+		else
+			printf("0 ");
+	}
+	return 0;
+}
+/*
+#include <iostream>
 #include<map>
 using namespace std;
 int main() {
@@ -6,13 +40,13 @@ int main() {
 	//int* stud;
 	//int* num;
 	map<int,int>num;
-	cin >> n >> m;
+	scanf("%d %d",&n,&m);
 	//stud = new int[n];
 	//num = new int[m];
 	for (int i = 0; i < n; i++) {
 		//cin >> stud[i];
 		int stud;
-		cin >> stud;
+		scanf("%d",&stud);
 		if (num.count(stud)) {
 			int t = num.find(stud)->second;
 			num.erase(stud);
@@ -24,7 +58,7 @@ int main() {
 	}
 	for (int i = 0; i < m; i++) {
 		int tmp;
-		cin >> tmp;
+		scanf("%d", &tmp);
 		if (num.count(tmp)) {
 			printf("%d ", num.find(tmp)->second);
 		}//num.insert({ tmp,true });
@@ -34,3 +68,4 @@ int main() {
 	}
 	return 0;
 }
+*/
