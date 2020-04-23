4e321dd558feecbc1fe8fcf46bef0c132f446276
maroro0220 <maro1339@gmail.com>
2020-04-23 13:22:31 +0900

    It's good

--- GroomVitaAlgo_WrongPrimeNum/GroomVitaAlgo_WrongPrimeNum/GroomVitaAlgo_WrongPrimeNum.cpp
+++ GroomVitaAlgo_WrongPrimeNum/GroomVitaAlgo_WrongPrimeNum/GroomVitaAlgo_WrongPrimeNum.cpp
@@ -2,6 +2,12 @@
 #include <cmath>
 using namespace std;
 long long* arr;
+int isPrime(long long n) {
+	for (long long i = 2; i * i <= n; i++) {
+		if (n % i == 0)return 0;
+	}
+	return 1;
+}
 int main() {
 	long long n;
 	scanf("%lld", &n);
@@ -9,27 +15,18 @@ int main() {
 		printf("0");
 		return 0;
 	}
-	arr = new long long[n + 1]();
-	//memset(arr, 1, n + 1);
-	int cnt = 0;
-	for (long long i = 2; i <= n; i++) {
-		if (arr[i] == 0) {
-			for (long long j = i * 2; j < n + 1; j += i) {
-				arr[j] = 1;
+	for (long long i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			if (isPrime(i) && isPrime(n / i)) {
+				printf("1");
+				return 0;
 			}
-			if ((n % i) == 0) {
-				cnt++;
-				if (cnt > 2) {
-					break;
-				}
+			else {
+				printf("0");
+				return 0;
 			}
 		}
 	}
-	if (cnt == 2) {
-		printf("1");
-	}
-	else {
-		printf("0");
-	}
+	printf("1");
 	return 0;
 }
