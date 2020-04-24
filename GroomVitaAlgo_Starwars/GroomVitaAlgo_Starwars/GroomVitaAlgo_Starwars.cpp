60e18c2b93eb27745ff63e35904983670c0e124f
maroro0220 <maro1339@gmail.com>
2020-04-24 16:41:56 +0900

    Greedy algorithm is impossible. Dynamic Prgramming is possible. This is other people's solution. I don't understand why sort is needed. Try again!!!

--- GroomVitaAlgo_Starwars/GroomVitaAlgo_Starwars/GroomVitaAlgo_Starwars.cpp
+++ GroomVitaAlgo_Starwars/GroomVitaAlgo_Starwars/GroomVitaAlgo_Starwars.cpp
@@ -5,28 +5,22 @@ int main() {
 	int n, k;
 	scanf("%d %d", &n, &k);
 	int* arr = new int[n]();
+	int* dp = new int[k+1];
+	fill(dp, dp + k+1, 10001);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
-	}
-	sort(arr, arr + n,greater<int>());
-	int cnt = 0;
-	bool chk = false;
-	for (int i = 0; i < n;) {
-		if (k - arr[i] > 0) {
-			k -= arr[i];
-			cnt++;
-		}
-		else if (k - arr[i] == 0) {
-			chk = true;
-		}
-		else {
-			i++;
+		if (arr[i] <= 10000) {
+			dp[arr[i]] = 1;
 		}
 	}
-	if (!chk) {
-		printf("-1");
+	sort(arr, arr + n);
+	for(int i=0;i<n;i++)
+		printf("%d ",arr[i]);
+	for (int i = 1; i <= k; i++) {
+		for (int j = 0; j < n && arr[j]<=i; j++) {
+			dp[i] = min(dp[i], dp[i - arr[j]]+1);
+		}
 	}
-	else {
-		printf("%d", cnt);
-	}return 0;
+	printf("%d", dp[k] == 10001 ? -1 : dp[k]);
+	return 0;
 }
